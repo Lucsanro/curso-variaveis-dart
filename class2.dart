e0a96eb807276bf8deb264354287a2087b9ee169
Lucca Rocha <lucca.rocha@outlook.com>
2021-08-22 15:04:27 -0300

    igual, diferente, maior que e menor que

---
+++ class2.dart
@@ -0,0 +1,42 @@
+void main() {
+  // IGUALDADE ==
+
+  int operador1 = 1;
+  int operador2 = 1;
+  double operador3 = 1.0;
+
+  bool resultado = operador1 == operador3;
+  print(resultado);
+
+  //DIFERENÇA !=
+  int operador4 = 1;
+  int operador5 = 2;
+  double operador6 = 3;
+
+  bool resultado1 = operador4 != operador5;
+  print(resultado1);
+
+  // MAIOR QUE >
+
+  int operador7 = 3;
+  int operador8 = 1;
+
+  bool resultado3 = operador7 > operador8;
+  print(resultado3);
+
+  // MAIOR QUE >
+
+  int operador9 = 2;
+  int operador10 = 3;
+
+  bool resultado4 = operador9 < operador10;
+  print(resultado4);
+
+  // MAIOR IGUAL >= , MENOR IGUAL <=
+
+  int operador11 = 5;
+  int operador12 = 5;
+
+  bool resultado5 = operador11 >= operador12;
+  print(resultado5);
+}
