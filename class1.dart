e1611a9f76bb064d26349354eca4bf18b898e05b
Lucca Rocha <lucca.rocha@outlook.com>
2021-08-22 14:44:59 -0300

    Desenvolvimento de código

---
+++ class1.dart
@@ -0,0 +1,15 @@
+void main() {
+  double valor1 = 15.63;
+  int valor2 = 30;
+
+  double resultado = valor1 + valor2;
+  print(resultado);
+  /*resultado+=1;
+  resultado = resultado + 1;
+  resultado--;
+  resultado++;
+  int resultado2 = 45 ~/ 10.4;
+  print(resultado2);*/
+  resultado = valor1 % valor2;
+  print(resultado);
+}
